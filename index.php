7870ee92bd7b8896a770190f4f990fd726deaadc
alfianubi <37077625+alfianubi@users.noreply.github.com>
2018-09-03 13:48:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Ubialmusic</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="#page-top">Ubialmusic</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <!-- li><a href="#about" class="page-scroll">About</a></li>
+        <li><a href="#restaurant-menu" class="page-scroll">Menu</a></li -->
+        <li><a href="#portfolio" class="page-scroll">Gallery</a></li>
+        <li><a href="#team" class="page-scroll">Video</a></li>
+        <li><a href="#contact" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+</nav>
+<!-- Header -->
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Ubialmusic</h1>
+            <p>Gallery / Video</p>
+            <a href="#portfolio" class="btn btn-custom btn-lg page-scroll">Show More</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- About Section -->
+<!--div id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 ">
+        <div class="about-img"><img src="img/about.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <h2>Our Restaurant</h2>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
+          <h3>Awarded Chefs</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Restaurant Menu Section -->
+<!--div id="restaurant-menu">
+  <div class="section-title text-center center">
+    <div class="overlay">
+      <h2>Menu</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed.</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Breakfast & Starters</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $35 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Main Course</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $45 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Dinner</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $45 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $350 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam.. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Coffee & Drinks</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $35 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Delicious Dish </div>
+            <div class="menu-item-price"> $30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="portfolio">
+  <div class="section-title text-center center">
+    
+      
+    </div>
+  </div>
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Instragram</h2>
+      <hr>
+      <p></p>
+    </div>
+    
+    <div class="row">
+      <?php
+          $json_url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=8446626533.8668403.4a26216906c049a2b20ec4aeb53d85d1";
+          $json = file_get_contents($json_url);
+          $link = json_decode($json, true);          
+          foreach($link['data'] as $item){
+            $thumbnail = $item['images']['low_resolution']['url'];
+            $caption = $item['caption']['text'];
+           /* $tag = $item['tags'][1];
+            $image_link = $item['images']['standard_resolution']['url'];
+            
+            $user= $item['user']['username'];*/
+
+        ?>
+              <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                  <a href="#"><img class="card-img-top" src=<?php echo $thumbnail ?> alt=""></a>
+                  <div class="card-body">
+                    <h4 class="card-title">
+                      <a href="#"><?php echo ""; ?></a>
+                    </h4>
+                    <p class="card-text"><?php echo "$caption"; ?></p>
+                  </div>
+                </div>
+              </div>
+        <?php
+        }
+        ?>
+      
+    </div>
+</div>
+
+
+<!-- Team Section -->
+<div id="team" class="text-center">
+  <div class="overlay">
+    <div class="container">
+            <!-- Page Heading -->
+      <div class="section-title text-center center">
+      <h2>Youtube</h2>
+      <hr>
+      <p></p>
+    </div>
+      <div class="row">
+        <?php
+          $json_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=konser+soundrenaline&type=video&key=AIzaSyA3MZ6y_FIqNwY9Fx3W17tWXScupTI7ktk&maxResults=9";
+          $json = file_get_contents($json_url);
+          $link = json_decode($json, true);
+          
+          foreach($link['items'] as $item){
+            $caption = $item['snippet']['title'];
+            $vid = $item['id']['videoId'];
+        ?>
+              <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                  <a href="#"><?php echo "<iframe class=card-img-top type='text/html' src='https://www.youtube.com/embed/$vid?controls=1' allowfullscreen></iframe>";?></a>
+                  <div class="card-body">
+                    <h4 class="card-title">
+                      <a href="#"><?php echo ""; ?></a>
+                    </h4>
+                    <p class="card-text"><?php echo "$caption"; ?></p>
+                  </div>
+                </div>
+              </div>
+        <?php
+        }
+        ?>
+      </div>
+      <!-- /.row -->
+    </div>
+  </div>
+</div>
+<!-- Call Reservation Section -->
+<!-- div id="call-reservation" class="text-center">
+  <div class="container">
+    <h2>Want to make a reservation? Call <strong>1-887-654-3210</strong></h2>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title text-center">
+      <h2>Contact Form</h2>
+      <hr>
+      <p>Leave a Message </p>
+    </div>
+    <div class="col-md-10 col-md-offset-1">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
+      </form>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-4">
+      <h3>Address</h3>
+      <div class="contact-item">
+        <p>Jalan Sadang Sedang No.41 RT.007/014 </p>
+        <p>Kel. Coblong Sekeloa, Kota Bandung Kode POS : 40134</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Tugas UAS Aplikasi Teknologi Online</h3>
+      <div class="contact-item">
+        <p>Name	: Alfian Al Ayubi</p>
+        <p>NIM	: 10115447</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Contact Info</h3>
+      <div class="contact-item">
+        <p>Phone: 085780943148</p>
+        <p>Email: alfianalubi@gmail.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center copyrights">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="social">
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+      </div>
+      <p>&copy; 2018 Ubialmusic. All rights reserved. Designed by <a href="http://www.templatewire.com" rel="nofollow">Alfian Al Ayubi</a></p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
